3a51e7aee088f3e25fbdea78006dfd1b3727d416
true377 <102599980+true377@users.noreply.github.com>
2022-04-13 00:22:53 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -59,55 +59,23 @@ $(function() {
 
 
     // Collapse
-    var accordion = (function (element) {
-  var _getItem = function (elements, className) { // функция для получения элемента с указанным классом
-    var element = undefined;
-    elements.forEach(function (item) {
-      if (item.classList.contains(className)) {
-        element = item;
-      }
-    });
-    return element;
-  };
-  return function () {
-    var _mainElement = {}, // .accordion
-      _item = {}, // .accordion-item
-      _content = {}; // .accordion-item-content
-    var _actionClick = function (e) {
-      if (!e.target.classList.contains('accordion_heaader')) { // прекращаем выполнение функции если кликнули не по заголовку
-        return;
-      }
-      e.preventDefault(); // отменям стандартное действие
-      // получаем необходимые данные
-      var header = e.target,
-        item = header.parentElement,
-        itemActive = _getItem(_item, 'active');
-      if (itemActive === undefined) { // добавляем класс show к элементу (в зависимости от выбранного заголовка)
-        item.classList.add('active');
-      } else {
-        // удаляем класс show у ткущего элемента
-        itemActive.classList.remove('active');
-        // если следующая вкладка не равна активной
-        if (itemActive !== item) {
-          // добавляем класс show к элементу (в зависимости от выбранного заголовка)
-          item.classList.add('active');
-        }
-      }
-    },
-    _setupListeners = function () {
-      // добавим к элементу аккордиона обработчик события click
-      _mainElement.addEventListener('click', _actionClick);
-    };
+   
+       
+       
+   $("[data-collapse]").ready(function(){
+    
 
-    return {
-      init: function (element) {
-        _mainElement = (typeof element === 'string' ? document.querySelector(element) : element);
-        _item = _mainElement.querySelectorAll('.accordion_item');
-        _setupListeners();
-      }
-    }
-  }
-})();
+    $(".accordion_item").eq(2).addClass("active");
+    $(".accordion_content").eq(2).show();
+
+    $(".accordion_heaader").click(function(){
+        $(this).next("p").slideToggle("slow")
+        .siblings("p:visible").slideUp("slow");
+        $(this).toggleClass("active");
+        $(this).siblings("h3").removeClass("active");
+    });
+
+});
 
 
 
